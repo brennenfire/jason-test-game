846175c081cc935113a09c0e9bd0820836ed506a
brennenfire <barbustefan2002@gmail.com>
2022-11-13 20:18:00 +0200

    published game
    at long last

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float jumpForce = 1f;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * speed;
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        Debug.Log($"Velocity = {rigidbody2D.velocity}");
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0; // ce e codul asta ba
+        animator.SetBool("Walk", walking); // animator.SetBool("Walk", horizontal != 0);
+
+        if (horizontal != 0)
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = horizontal < 0;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
