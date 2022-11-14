b076ff14a8c59c7dc54506771a154a1e202a23b2
brennenfire <barbustefan2002@gmail.com>
2022-11-14 21:36:00 +0200

    added direction and speed

--- Platformer/Assets/Scripts/Fly.cs
+++ Platformer/Assets/Scripts/Fly.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
     Vector2 startingPosition;
-    Vector2 direction = Vector2.up;
-
+    [SerializeField] Vector2 direction = Vector2.up;
+    [SerializeField] float maxDistance = 2;
+    [SerializeField] float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +15,11 @@ public class Fly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * Time.deltaTime);
+        transform.Translate(direction.normalized * Time.deltaTime * speed);
         var distance = Vector2.Distance(startingPosition, transform.position);
-        if(distance >= 2)
+        if(distance >= maxDistance)
         {
+            transform.position = startingPosition + (direction.normalized * maxDistance);
             direction *= -1;
         }
     }
