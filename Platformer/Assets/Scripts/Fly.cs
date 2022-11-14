661663853f2b842322677b9e892a258a7c71e0a0
brennenfire <barbustefan2002@gmail.com>
2022-11-14 23:17:14 +0200

    separated fly into two scripts added kill on enter

--- Platformer/Assets/Scripts/Fly.cs
+++ Platformer/Assets/Scripts/Fly.cs
@@ -24,12 +24,4 @@ public class Fly : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        var player = collision.GetComponent<Player>();
-        if(player != null)
-        {
-            player.ResetToStart();
-        }
-    }
 }
