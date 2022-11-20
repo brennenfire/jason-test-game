383c4ad5bc94f30781d2d42caa3a0dbe2d49610b
brennenfire <barbustefan2002@gmail.com>
2022-11-20 20:09:11 +0200

    added raycasts and debugging to our slime

--- Platformer/Assets/Scripts/Slime.cs
+++ Platformer/Assets/Scripts/Slime.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform leftSensor;
+    [SerializeField] Transform rightSensor;
+
     new Rigidbody2D rigidbody2D;
+    SpriteRenderer sprite;
+    float direction = -1;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        rigidbody2D.velocity = Vector2.left;
+        Debug.DrawRay(leftSensor.position, Vector2.down * 0.1f, Color.red);
+        Debug.DrawRay(rightSensor.position, Vector2.down * 0.1f, Color.red);
+        if (direction < 0)
+        {
+            rigidbody2D.velocity = new Vector2(direction, rigidbody2D.velocity.y);
+            var result = Physics2D.Raycast(leftSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+            {
+                TurnAround();
+            }
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(direction, rigidbody2D.velocity.y);
+            var result = Physics2D.Raycast(rightSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+            {
+                TurnAround();
+            }
+        }
+    }
+
+    void TurnAround()
+    {
+        direction *= -1;
+        sprite.flipX = direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
