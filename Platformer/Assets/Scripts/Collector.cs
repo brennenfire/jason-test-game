a7b2f34de62baac09850b8aeec4242d058e014c2
brennenfire <barbustefan2002@gmail.com>
2022-12-03 21:00:20 +0200

    added some ui elements to collector

--- Platformer/Assets/Scripts/Collector.cs
+++ Platformer/Assets/Scripts/Collector.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] collectibles;
+    TMP_Text remainingText;
 
+    void Start()
+    {
+        remainingText = GetComponentInChildren<TMP_Text>();
+    }
     void Update()
     {
+        int countRemaining = 0;
         foreach(var collectible in collectibles) 
         {
             if(collectible.isActiveAndEnabled) 
             {
-                return;
+                countRemaining++;
             }
+        }
+        remainingText?.SetText(countRemaining.ToString());
+
+        if (countRemaining > 0) 
+        {
+            return;
         }
 
         Debug.Log("Got all gems");
