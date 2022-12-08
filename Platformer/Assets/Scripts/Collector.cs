c1946266fa890bf4ad3b4a8b22b5d19c049e092b
brennenfire <barbustefan2002@gmail.com>
2022-12-08 21:03:29 +0200

    added draw line gizmo for collector script

--- Platformer/Assets/Scripts/Collector.cs
+++ Platformer/Assets/Scripts/Collector.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,11 +11,12 @@ public class Collector : MonoBehaviour
 {
     [SerializeField] List<Collectible> collectibles;
     [SerializeField] UnityEvent collectionComplete;
+    static Color gizmoColor = new Color(0.61f, 0.61f, 0.61f, 1);
 
     TMP_Text remainingText;
     int countCollected;
     int countRemaining;
-
+    
     void Start()
     {
         remainingText = GetComponentInChildren<TMP_Text>();
@@ -43,6 +45,40 @@ public class Collector : MonoBehaviour
     void OnValidate()
     {
         collectibles = collectibles.Distinct().ToList();
+    }
+
+    /*
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+        foreach (var collectible in collectibles) 
+        {
+            Gizmos.DrawLine(transform.position, collectible.transform.position);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        foreach (var collectible in collectibles)
+        {
+            Gizmos.DrawLine(transform.position, collectible.transform.position);
+        }
+    }
+    */
+    void OnDrawGizmos()
+    {
+        foreach(var collectible in collectibles) 
+        {
+            if(UnityEditor.Selection.activeGameObject == gameObject) 
+            {
+                Gizmos.color = Color.yellow;
+            }
+            else
+            {
+                Gizmos.color = gizmoColor;
+            }
+            Gizmos.DrawLine(transform.position, collectible.transform.position);
+        }    
     }
 
 }
