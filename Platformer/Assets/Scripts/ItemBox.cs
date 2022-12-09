f6c555ebc731bf76b752a704d20c4f35c759e5fd
brennenfire <barbustefan2002@gmail.com>
2022-12-09 19:17:40 +0200

    made item box implement our base class

--- Platformer/Assets/Scripts/ItemBox.cs
+++ Platformer/Assets/Scripts/ItemBox.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemBox : HittableFromBelow
@@ -16,30 +14,21 @@ public class ItemBox : HittableFromBelow
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    protected override bool CanUse => used == false && item != null;
+    protected override void Use()
     {
-        if (used)
+        if(item == null)
         {
             return;
         }
-        var player = collision.collider.GetComponent<Player>();
-        if (player == null)
+        base.Use();
+        used = true;
+        item.SetActive(true);
+        var itemRigidbody = item.GetComponent<Rigidbody2D>();
+        if (itemRigidbody != null)
         {
-            return;
-        }
-        if (collision.contacts[0].normal.y > 0)
-        {
-            GetComponent<SpriteRenderer>().sprite = usedSprite;
-            if (item != null)
-            {
-                used = true;
-                item.SetActive(true);
-                var itemRigidbody = item.GetComponent<Rigidbody2D>();
-                if(itemRigidbody != null)
-                {
-                    itemRigidbody.velocity = itemLaunchVelocity;
-                }
-            }
+            itemRigidbody.velocity = itemLaunchVelocity;
         }
     }
+
 }
