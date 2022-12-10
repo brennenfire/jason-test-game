498699cbcd2422fa94a2ad9f183e57788f246bcb
brennenfire <barbustefan2002@gmail.com>
2022-12-11 00:07:55 +0200

    made switch work great

--- Platformer/Assets/Scripts/Switch.cs
+++ Platformer/Assets/Scripts/Switch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,15 @@ public class Switch : MonoBehaviour
     [SerializeField] Sprite leftSwitch;
     [SerializeField] UnityEvent onLeft;
     [SerializeField] UnityEvent onRight;
+    Collider2D col;
+    SpriteRenderer sprite;
 
+    private void Start()
+    {
+        col = GetComponent<Collider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    /*
     void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.collider.GetComponent<Player>();
@@ -25,6 +34,35 @@ public class Switch : MonoBehaviour
         }
         if (collision.contacts[0].normal.x < 0)
         {
+            sprite.sprite = leftSwitch;
+            onLeft.Invoke();
+        }
+    }
+    */
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        
+        if (player == null)
+        {
+            return;
+        }
+        col.isTrigger = true;
+        FindSide(collision);
+    }
+
+    private void FindSide(Collider2D collision)
+    {
+        if (collision.transform.position.x < col.transform.position.x)
+        {
+            Debug.Log("stanga");
+            sprite.sprite = rightSwitch;
+            onRight.Invoke();
+        }
+        if (collision.transform.position.x > col.transform.position.x)
+        {
+            Debug.Log("dreapta");
             sprite.sprite = leftSwitch;
             onLeft.Invoke();
         }
