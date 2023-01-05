16b038d944be0f8b22fb5476327c1e67323e5061
brennenfire <barbustefan2002@gmail.com>
2023-01-05 23:57:48 +0200

    new panel graphics and toggle and onvalidate(?)

--- Platformer/Assets/Scripts/UIStartLevelButton.cs
+++ Platformer/Assets/Scripts/UIStartLevelButton.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIStartLevelButton : MonoBehaviour
 {
     [SerializeField] string levelName;
 
+    private void OnValidate()
+    {
+        GetComponentInChildren<TMP_Text>()?.SetText(levelName);
+    }
     public void LoadLevel()
     {
         SceneManager.LoadScene(levelName);
