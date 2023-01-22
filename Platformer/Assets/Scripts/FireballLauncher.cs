bf4e75513930aba22add7a3301f5618a2fb95969
brennenfire <barbustefan2002@gmail.com>
2023-01-22 22:38:24 +0200

    launching fireballs and reading from input

--- Platformer/Assets/Scripts/FireballLauncher.cs
+++ Platformer/Assets/Scripts/FireballLauncher.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FireballLauncher : MonoBehaviour
 {
     [SerializeField] Fireball fireballPrefab;
+    [SerializeField] Player player;
+    string fireButton;
 
     private void Start()
     {
-        Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+        fireButton = $"P{player.PlayerNumber}Fire";
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown(fireButton))
+        {
+            Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
